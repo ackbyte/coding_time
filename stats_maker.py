f4f99baca8f2a07a3ae8ec2252d21fea41bd984a
Shadow <shadow2302ezg@gmail.com>
2019-01-09 19:03:29 +0100

    09.10

--- stats_maker.py
+++ stats_maker.py
@@ -1,24 +1,40 @@
+from glob import glob
+from pprint import pprint
+import os
+
 stats = {}
 
-with open("themes.txt", "r", encoding="utf-8") as file:
-    for line in file:
-        line = line.strip()
-        numb, yesno = line.split("=")
-        stats.setdefault(numb, [])
-        stats[numb].append(yesno)
+users = glob("./course_1/users/*")
 
-with open("users/Shadow2302", "r", encoding="utf-8") as file:
-    for line in file:
-        line = line.strip()
-        numb, yesno = line.split("=")
-        stats.setdefault(numb, [])
-        stats[numb].append(yesno)
+for user in users:
+    stats.setdefault('0', [" "])
+    stats['0'].append(os.path.basename(user))
 
-with open("users/tromboid", "r", encoding="utf-8") as file:
-    for line in file:
-        line = line.strip()
-        numb, yesno = line.split("=")
-        stats.setdefault(numb, [])
-        stats[numb].append(yesno)
 
-print(stats)
+def read_file(filename):
+    with open(filename, "r", encoding="utf-8") as file:
+        for line in file:
+            line = line.strip()
+            numb, yesno = line.split("=")
+            stats.setdefault(numb, [])
+            stats[numb].append(yesno)
+
+
+read_file("./course_1/themes.txt")
+
+for user in users:
+    read_file(user)
+
+lengths = []
+
+# Creating stats
+
+text = ""
+
+for lesson in stats:
+    line = " | ".join(stats[lesson])
+    text += "\n| {} | {} |".format(lesson, line)
+
+
+with open("./course_1/stats.txt", "w", encoding="utf-8") as file:
+    file.write(text)
